35dee3606b9eeb12008dc1fee6558bcd9abf4481
JS-120 <js_@outlook.jp>
2024-08-15 17:12:29 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const homeElement = document.getElementById('home');
+const toolElement = document.getElementById('tool');
+const blogElement = document.getElementById('blog');
+
+window.onload = () => {
+  homeElement.style.display = "block";
+  toolElement.style.display = "none";
+  blogElement.style.display = "none";
+}
+function home() {
+  homeElement.style.display = "block";
+  toolElement.style.display = "none";
+  blogElement.style.display = "none";
+}
+function tool() {
+  homeElement.style.display = "none";
+  toolElement.style.display = "block";
+  blogElement.style.display = "none";
+}function blog() {
+  homeElement.style.display = "none";
+  toolElement.style.display = "none";
+  blogElement.style.display = "block";
+}
